b23915b3ed3a912de2ed5759fe649e7358047eba
Valber Junior <84424883+ValberJunior@users.noreply.github.com>
2023-11-29 16:16:19 -0300

    update signinPage

--- packages/core-components/src/layout/SignInPage/SignInPage.tsx
+++ packages/core-components/src/layout/SignInPage/SignInPage.tsx
@@ -36,6 +36,7 @@ import BackstageLogo from "./assets/backstage.png";
 import KeycloakLogo from "./assets/keycloak.png";
 import OktaLogo from "./assets/okta.png";
 import GithubLogo from "./assets/github.png";
+import GitlabLogo from "./assets/gitlab.png"
 
 
 type MultiSignInPageProps = SignInPageProps & {
@@ -192,6 +193,7 @@ export const SingleSignInPage = ({
                   {provider.title === "Keycloak" && <img src={KeycloakLogo} alt={provider.title} className={classes.providerLogo}/>}
                   {provider.title === "Okta" && <img src={OktaLogo} alt={provider.title} className={classes.providerLogo} />}
                   {provider.title === "GitHub" && <img src={GithubLogo} alt={provider.title} className={classes.providerLogo} />}
+                  {provider.title === "Gitlab" && <img src={GitlabLogo} alt={provider.title} className={classes.providerLogo} />}
                   <h3>{provider.message}</h3>
                 </div>
                 {error && error.name !== 'PopupRejectedError' && (
